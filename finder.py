7a0a96c062c727c54bb5f592fb2977e7d9f2b1ba
Dankib16 <dankibish@gmail.com>
2023-11-16 13:25:47 +0200

    Added option to choose page_size and tlds

--- finder.py
+++ finder.py
@@ -1,4 +1,7 @@
+from ast import main
+from typing import Optional
 import typer
+from rich import print 
 
 from lib.daddy import Daddy
 from lib.ai import AI
@@ -7,20 +10,38 @@ app = typer.Typer()
 daddy = Daddy()
 
 
+
+
+
+
 @app.command()
 def check_domain(domain: str):
     daddy.print(f"{':white_heavy_check_mark:' if daddy.is_available(domain) else ':cross_mark:'} {domain} is {'NOT ' if not daddy.is_available(domain) else ''}available")
 
 
 @app.command()
-def search(desc: str, page_size=10):
+def search(desc: str, page_size: Optional[str] = 10,tlds: Optional[str]= '.com' '.ai' '.io'):
+    
     ai = AI()
-    domains = ai.next(desc)
+    domains = ai.next(desc,page_size,tlds)
     # TODO: Add `in loop` search asking for feedback
     # Implement chat-like behavior
     for domain in domains:
-        check_domain(domain)
+        check_domain(domain)  
+
+
+
+
+
+
+
+
+        
+
+
 
 
 if __name__ == "__main__":
+    
     app()
+    
